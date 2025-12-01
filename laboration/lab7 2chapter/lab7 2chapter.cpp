3aaf5b2fe04513ed80d57ca0bf4a2ad9d2ffcbf8
AniEndrIgo <androsov.endrigo75@mail.ru>
2025-12-01 21:54:14 +0200

    +lab7

---
+++ laboration/lab7 2chapter/lab7 2chapter.cpp
@@ -0,0 +1,133 @@
+﻿#include <iostream>
+#include <array>
+void sortcopy(std::array<int, 10> arr, bool var) {
+    for (int i = 0; i < arr.size() - 1; i++) {
+        for (int j = 0; j < arr.size() - i - 1; j++) {
+            bool swap = false;
+
+            switch (var) {
+            case true:
+                if (arr[j] > arr[j + 1]) swap = true;
+                break;
+            case false:
+                if (arr[j] < arr[j + 1]) swap = true;
+                break;
+            }
+
+            if (swap) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    for (int num : arr) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+}
+void sortoriginal(std::array<int, 10>& arr, bool var) {
+    for (int i = 0; i < arr.size() - 1; i++) {
+        int minmax = i;
+        for (int j = i + 1; j < arr.size(); j++) {
+            switch (var) {
+            case true:
+                if (arr[j] < arr[minmax]) minmax = j;
+                break;
+            case false:
+                if (arr[j] > arr[minmax]) minmax = j;
+                break;
+            }
+        }
+        if (minmax != i) {
+            int dos = arr[i];
+            arr[i] = arr[minmax];
+            arr[minmax] = dos;
+        }
+    }
+    for (int num : arr) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+}
+void sortukaz(std::array<int, 10>* arr, bool var) {
+    for (int i = 1; i < arr->size(); i++) {
+        int h = (*arr)[i];
+        int j = i - 1;
+        while (j >= 0) {
+            bool swap = false;
+            switch (var) {
+            case true:
+                if ((*arr)[j] > h) swap = true;
+                break;
+            case false:
+                if ((*arr)[j] < h) swap = true;
+                break;
+            }
+            if (!swap) break;
+            (*arr)[j + 1] = (*arr)[j];
+            j--;
+        }
+        (*arr)[j + 1] = h;
+    }
+    for (int num : *arr) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+}
+void print(const std::array<int, 10>& arr) {
+    for (int num : arr) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+}
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    std::srand(std::time(nullptr));
+    std::array<int, 10> mss;
+    for (int i = 0; i < 10; i++) {
+        mss[i] = std::rand() % 21 - 10;
+    }
+    std::cout << "Массив случайных чисел от -10 до 10:" << std::endl;
+    for (int i = 0; i < 10; i++) {
+        std::cout << mss[i] << " ";
+    }
+    std::cout << std::endl;
+    std::array<int, 10> original = mss;
+    std::cout << "\n1.ПЕРЕДАЧА ПО ЗНАЧЕНИЮ:" << std::endl;
+    std::cout << "-------------------------------------------------" << std::endl;
+    std::cout << "исходный массив:";
+    print(original);
+    std::cout << "сортировка по возрастанию:";
+    sortcopy(original, true);
+    std::cout << "сортировка по убыванию:";
+    sortcopy(original, false);
+    std::cout << "исходный массив теперь: ";
+    print(original);
+    std::cout << "Массив не изменился тк функция изменила копию" << std::endl;
+    std::cout << "\n2.ПЕРЕДАЧА ПО ССЫЛКЕ:" << std::endl;
+    std::cout << "-------------------------------------------" << std::endl;
+    original = mss;
+    std::cout << "исходный массив:";
+    print(original);
+    std::cout << "сортировка по возрастанию:";
+    sortoriginal(original, true);
+    std::cout << "сортировка по убыванию:";
+    sortoriginal(original, false);
+    std::cout << "исходный массив теперь: ";
+    print(original);
+    std::cout << "Массив изменился тк работали с оригиналом" << std::endl;
+    std::cout << "\n3.ПЕРЕДАЧА ПО УКАЗАТЕЛЮ:" << std::endl;
+    std::cout << "------------------------------------------------" << std::endl;
+    original = mss;
+    std::cout << "исходный массив:";
+    print(original);
+    std::cout << "сортировка по возрастанию:";
+    sortukaz(&original, true);
+    std::cout << "сортировка по убыванию:";
+    sortukaz(&original, false);
+    std::cout << "исходный массив теперь: ";
+    print(original);
+    std::cout << "Массив изменился тк работали с оригиналом" << std::endl;
+}
---
+++ laboration/lab7/lab7.cpp
@@ -0,0 +1,123 @@
+﻿#include <vector>
+#include <iostream>
+
+std::vector<int> element(std::vector<int>& vec) {
+    std::vector<int> vector = vec;
+    for (int i = 0; i < vector.size(); i++) {
+        if (vector[i] % 2 == 0) {
+            vector[i] *= 2;
+        }
+        else {
+            vector[i] *= -1;
+        }
+    }
+    return vector;
+}
+void print(const std::vector<int>& vec) {
+    for (int xmss : vec) {
+        std::cout << xmss << " ";
+    }
+}
+void plusbegin(std::vector<int>& vec, int b) {
+    vec.insert(vec.begin(), b);
+}
+void write(std::vector<int>& vec, int g) {
+    vec.reserve(vec.size() + 1);
+    vec.push_back(g);
+}
+int main()
+{
+    std::setlocale(LC_ALL, "Russian");
+    std::vector<int> mss = { 1, 32, 4 };
+    std::vector<int> first_mss = mss;
+    int a;
+    do {
+        std::cout << "выбери действие\nвыйти = 0\nпросмотр массива = 1\nдобавить число в начало = 2\nдобавить число в конец массива = 3\nОчистка всего массива = 4\nПоиск элемента в массиве = 5\nВывести до/после = 6" << std::endl;
+        std::cin >> a;
+        if (a == 0) {
+            std::cout << "Результат до/после редактирования четных и нечетных чисел) соответсвенно: ";
+            print(first_mss);
+            std::cout << "/ ";
+            print(element(mss));
+            std::cout << "Пока";
+            return 0;
+        }
+        if (a == 1) {
+            system("cls");
+            std::cout << "массив: ";
+            print(mss);
+            std::cout << std::endl;
+            std::cout << "массив(ред. числа): ";
+            print(element(mss));
+            std::cout << std::endl;
+            std::cout << "----------------------------------------------------" << std::endl;
+        }
+        if (a == 2) {
+            system("cls");
+            std::cout << "введите число: ";
+            int b;
+            std::cin >> b;
+            plusbegin(mss, b);
+            std::cout << std::endl;
+        }
+        if (a == 3) {
+            system("cls");
+            std::cout << "введите число: ";
+            int g;
+            std::cin >> g;
+            write(mss, g);
+            system("cls");
+        }
+        if (a == 4) {
+            system("cls");
+            mss.clear();
+            std::cout << "Массив очищен";
+            std::cout << std::endl;
+            std::cout << "----------------------------------------------------" << std::endl;
+        }
+        if (a == 5) {
+            system("cls");
+            if (mss.size() == 0) {
+                std::cout << "массив пуст\n";
+                std::cout << "----------------------------------------------------" << std::endl;
+                continue;
+            }
+            int n;
+            std::cout << "Введите количество выводимых чисел: ";
+            std::cin >> n;
+            while (n < 0) {
+                std::cout << "Бро число не должно быть отрицательным: ";
+                std::cin >> n;
+            }
+            std::vector<int> ind(n);
+            std::cout << "Введите индексы чисел: ";
+            for (int i = 0; i < n; i++) {
+                int index;
+                std::cin >> index;
+                while (index >= mss.size()) {
+                    std::cout << "такого индекса нету в массиве(введи заного)";
+                    std::cin >> index;
+                }
+                ind[i] = index;
+            }
+            for (int i = 0; i < ind.size(); i++) {
+                int index = ind[i];
+                std::cout << mss[index] << " ";
+            }
+            std::cout << std::endl;
+            std::cout << "----------------------------------------------------" << std::endl;
+        }
+        if (a == 6) {
+            system("cls");
+            std::cout << "Результат до/после соответсвенно: ";
+            print(first_mss);
+            std::cout << "/ ";
+            if (mss.size() == 0) {
+                std::cout << "массив пуст";
+            }
+            print(element(mss));
+            std::cout << "\n----------------------------------------------------" << std::endl;
+        }
+    } while (true);
+
+}
